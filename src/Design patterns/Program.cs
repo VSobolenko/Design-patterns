bbb40e44fa78f81f5ca482eb210ef487b5a885d2
Vladislav Sobolenko2 <v.sobolenko.vs@gmail.com>
2021-10-12 00:01:08 +0300

    test factory method

--- src/Design patterns/Program.cs
+++ src/Design patterns/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Design_patterns.Factories;
 
 namespace Design_patterns
 {
@@ -6,7 +6,25 @@ namespace Design_patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lenovo = new LenovoFactory();
+            lenovo.GetPhone("A7000");
+            lenovo.GetPhone("S304", "Legion");
+            lenovo.GetPhone("S60", "Vibe");
+
+            var lg = new LGFactory();
+            lg.GetPhone("NDzk31", "Velvet");
+            lg.GetPhone("Z30", "K61DualSim");
+            lg.GetPhone("Ty130", "Wing");
+
+            var samsung = new SamsungFactory();
+            samsung.GetPhone("Prime", "Grand");
+            samsung.GetPhone("S9", "Galaxy");
+            samsung.GetPhone("Z Flip", "Galaxy.Fold");
+
+            var xiaomi = new XiaomiFactory();
+            xiaomi.GetPhone("7", "RedmiNote");
+            xiaomi.GetPhone("9T", "Redmi");
+            xiaomi.GetPhone("11 Ultra", "Mi");
         }
     }
 }
