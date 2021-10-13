f7a024f0807ac0057bc5fc08a73f21aab6f31c2e
Vladislav Sobolenko2 <v.sobolenko.vs@gmail.com>
2021-10-14 02:30:14 +0300

    test abstract factory

--- src/Design patterns/Program.cs
+++ src/Design patterns/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Design_patterns.CarFactories;
+using Design_patterns.Cars;
+using System;
 
 namespace Design_patterns
 {
@@ -6,7 +8,29 @@ namespace Design_patterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CarFactory bmw = new BMWFactory();
+            Car coupeBMW = bmw.GetCar(CarType.Coupe);
+            Car hatchbackBMW = bmw.GetCar(CarType.Hatchback);
+
+            CarFactory toyota = new ToyotaFactory();
+            Car hatchbackToyota = toyota.GetCar(CarType.Hatchback);
+            Car minivanToyota = toyota.GetCar(CarType.Minivan);
+
+            CarFactory volkswagen = new VolkswagenFactory();
+            Car minivanVolkswagen = volkswagen.GetCar(CarType.Minivan);
+            Car pickupVolkswagen = volkswagen.GetCar(CarType.Pickup);
+
+            PrintAbotCars(coupeBMW, hatchbackBMW, hatchbackToyota
+                , minivanToyota, minivanVolkswagen, pickupVolkswagen);
+        }
+
+        static void PrintAbotCars(params Car[] cars)
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.ToString());
+                Console.WriteLine();
+            }
         }
     }
 }
