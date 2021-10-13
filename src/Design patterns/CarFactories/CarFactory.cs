1ed551365799a01e55ac0d2ceed59c9d3794931b
Vladislav Sobolenko2 <v.sobolenko.vs@gmail.com>
2021-10-14 02:29:49 +0300

    added car factories

---
+++ src/Design patterns/CarFactories/CarFactory.cs
@@ -0,0 +1,16 @@
+﻿using Design_patterns.Cars;
+
+namespace Design_patterns.CarFactories
+{
+    abstract class CarFactory
+    {
+        public Car GetCar(CarType type)
+        {
+            Car car = CreateCar(type);
+            car.Configure();
+
+            return car;
+        }
+        protected abstract Car CreateCar(CarType type);
+    }
+}
---
+++ src/Design patterns/CarFactories/ToyotaFactory.cs
@@ -0,0 +1,37 @@
+﻿using Design_patterns.Cars;
+using Design_patterns.Cars.Types;
+using Design_patterns.ComponentFactories;
+
+namespace Design_patterns.CarFactories
+{
+    class ToyotaFactory : CarFactory
+    {
+        protected override Car CreateCar(CarType type)
+        {
+            Manufacturer manufacturer = Manufacturer.Toyota;
+
+            ICompositionFactory factory = new StandardQualityFactory();
+
+            switch (type)
+            {
+                case CarType.Coupe:
+                    return new Coupe(factory, manufacturer);
+                case CarType.Hatchback:
+                    return new Hatchback(factory, manufacturer);
+                case CarType.Micro:
+                    return new Micro(factory, manufacturer);
+                case CarType.Minivan:
+                    return new Minivan(factory, manufacturer);
+                case CarType.Pickup:
+                    return new Pickup(factory, manufacturer);
+                case CarType.Sedan:
+                    return new Sedan(factory, manufacturer);
+                case CarType.Truck:
+                    return new Truck(factory, manufacturer);
+                default:
+                    break;
+            }
+            return null;
+        }
+    }
+}
